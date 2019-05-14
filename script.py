29d4d9ff5739167991b09558caeb6bf6ab12fc3e
aleong1 <aleong1@stuy.edu>
2019-05-13 21:55:53 -0400

    nksdvnsnonoei

--- script.py
+++ script.py
@@ -12,7 +12,7 @@ def run(filename):
     if p:
         (commands, symbols) = p
     else:
-        print "Parsing failed."
+        print ("Parsing failed.")
         return
 
     view = [0,
@@ -46,10 +46,64 @@ def run(filename):
                           'blue': [0.2, 0.5, 0.5]}]
     reflect = '.white'
 
-    print symbols
+    polygons = []
     for command in commands:
-        print command
 
-        
+        if command['op'] == "push":
+            stack.append([x[:] for x in stack[-1]])
+
+        elif command['op'] == "pop":
+            stack.pop()
+
+        elif command['op'] == "move":
+            t = make_translate(command['args'][0], command['args'][1], command['args'][2])
+            matrix_mult( stack[-1], t )
+            stack[-1] = [ x[:] for x in t]
+
+        elif command['op'] == "rotate":
+            theta = float(command['args'][1]) * (math.pi / 180)
+            if command['args'][0] == 'x':
+                t = make_rotX(theta)
+            elif command['args'][0] == 'y':
+                t = make_rotY(theta)
+            else:
+                t = make_rotZ(theta)
+            matrix_mult( stack[-1], t )
+            stack[-1] = [ x[:] for x in t]
+
+        elif command['op'] == "scale":
+            t = make_scale(command['args'][0], command['args'][1], command['args'][2])
+            matrix_mult( stack[-1], t )
+            stack[-1] = [ x[:] for x in t]
+
+        elif command['op'] == "box" or command['op'] == "sphere" or command['op'] == "torus":
+            if command['op'] == "box":
+                add_box(polygons,
+                        float(command['args'][0]), float(command['args'][1]), float(command['args'][2]),
+                        float(command['args'][3]), float(command['args'][4]), float(command['args'][5]))
+
+            elif command['op'] == "sphere":
+                add_sphere(polygons,
+                           float(command['args'][0]), float(command['args'][1]), float(command['args'][2]),
+                           float(command['args'][3]), step_3d)
+
+            elif command['op'] == "torus":
+                add_torus(polygons,
+                          float(command['args'][0]), float(command['args'][1]), float(command['args'][2]),
+                          float(command['args'][3]), float(command['args'][4]), step_3d)
+
+            matrix_mult( stack[-1], polygons )
+
+            if command['constants']:
+                draw_polygons(polygons, screen, zbuffer, view, ambient, light, symbols, command['constants'])
+            else:
+                draw_polygons(polygons, screen, zbuffer, view, ambient, light, symbols, reflect)
     
-        
+            polygons = []
+
+
+        elif command['op'] == "line":
+
+        elif command['op'] == "save":
+
+        elif command['op'] == "display":
